df011818cd7001e30c1f95ef6d14ac0cd195f69c
Aarti <160009759+aartichoudhary002@users.noreply.github.com>
2024-10-09 12:29:17 +0000

    commited split

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,12 +1,12 @@
 
-const name = "hitesh"
+const name = "aarti"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('aarti-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -17,20 +17,20 @@ const gameName = new String('hitesh-hc-com')
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4)  //not valid negative value
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-8, 4)  // we can add negative value 
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   aarti    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());  // removing starting and ending space
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://aarti.com/aarti%20choudhary"
 
-console.log(url.replace('%20', '-'))
+console.log(url.replace('%20', '-'))   //replacing %20 to -
 
-console.log(url.includes('sundar'))
+console.log(url.includes('sundar'))  //return type is boolean
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-'));   
